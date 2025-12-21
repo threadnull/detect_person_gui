d6adfab4fc79328a8b0aa208caf38fa8895f8e35
threadnull <threadnull@gmail.com>
2025-12-21 18:00:33 +0900

    Main Update

--- main.py
+++ main.py
@@ -50,7 +50,10 @@ class MainWindow(QMainWindow):
             
             plotted_img = results[0].plot()
             
+            # BGR -> RBG
             rgb_img = plotted_img[..., ::-1].copy()
+
+            # qt 이미지로 변환
             h, w, ch = rgb_img.shape
             bytes_per_line = ch * w
             q_img = QImage(rgb_img.data, w, h, bytes_per_line, QImage.Format_RGB888)
